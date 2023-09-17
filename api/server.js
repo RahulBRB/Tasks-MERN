d8ded02d1747bc2f432eb1899342db3742827060
Rahul Baskey <rahulblue17@gmail.com>
2023-09-17 16:35:38 +0530

    some changes in serverjs

--- api/server.js
+++ api/server.js
@@ -4,4 +4,11 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:5000", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(() => console.log("connection established")).catch(console.error);
+
+app.listen(3001, () => console.log("server started on port 3001"));
