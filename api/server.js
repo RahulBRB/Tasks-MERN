b1ef6fd103efa63836d38ffb544156374b50bd3a
Rahul Baskey <rahulblue17@gmail.com>
2023-09-17 16:48:45 +0530

    changes

--- api/server.js
+++ api/server.js
@@ -6,11 +6,3 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:5000", {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-    .then(() => console.log("connection established"))
-    .catch(console.error);
-
-app.listen(3001, () => console.log("server started on port 3001"));
